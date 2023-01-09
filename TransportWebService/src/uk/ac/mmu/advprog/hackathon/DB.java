09adb62391c61400b5b27d4efd8811da1870c2f2
Ben <none>
2023-01-09 10:31:40 +0000

    Imported startercode

---
+++ TransportWebService/src/uk/ac/mmu/advprog/hackathon/DB.java
@@ -0,0 +1,77 @@
+package uk.ac.mmu.advprog.hackathon;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Handles database access from within your web service
+ * @author You, Mainly!
+ */
+public class DB implements AutoCloseable {
+	
+	//allows us to easily change the database used
+	private static final String JDBC_CONNECTION_STRING = "jdbc:sqlite:./data/NaPTAN.db";
+	
+	//allows us to re-use the connection between queries if desired
+	private Connection connection = null;
+	
+	/**
+	 * Creates an instance of the DB object and connects to the database
+	 */
+	public DB() {
+		try {
+			connection = DriverManager.getConnection(JDBC_CONNECTION_STRING);
+		}
+		catch (SQLException sqle) {
+			error(sqle);
+		}
+	}
+	
+	/**
+	 * Returns the number of entries in the database, by counting rows
+	 * @return The number of entries in the database, or -1 if empty
+	 */
+	public int getNumberOfEntries() {
+		int result = -1;
+		try {
+			Statement s = connection.createStatement();
+			ResultSet results = s.executeQuery("SELECT COUNT(*) AS count FROM Stops");
+			while(results.next()) { //will only execute once, because SELECT COUNT(*) returns just 1 number
+				result = results.getInt(results.findColumn("count"));
+			}
+		}
+		catch (SQLException sqle) {
+			error(sqle);
+			
+		}
+		return result;
+	}
+	
+	/**
+	 * Closes the connection to the database, required by AutoCloseable interface.
+	 */
+	@Override
+	public void close() {
+		try {
+			if ( !connection.isClosed() ) {
+				connection.close();
+			}
+		}
+		catch(SQLException sqle) {
+			error(sqle);
+		}
+	}
+
+	/**
+	 * Prints out the details of the SQL error that has occurred, and exits the programme
+	 * @param sqle Exception representing the error that occurred
+	 */
+	private void error(SQLException sqle) {
+		System.err.println("Problem Opening Database! " + sqle.getClass().getName());
+		sqle.printStackTrace();
+		System.exit(1);
+	}
+}
---
+++ TransportWebService/src/uk/ac/mmu/advprog/hackathon/TransportWebService.java
@@ -0,0 +1,38 @@
+package uk.ac.mmu.advprog.hackathon;
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+/**
+ * Handles the setting up and starting of the web service
+ * You will be adding additional routes to this class, and it might get quite large
+ * Feel free to distribute some of the work to additional child classes, like I did with DB
+ * @author You, Mainly!
+ */
+public class TransportWebService {
+
+	/**
+	 * Main program entry point, starts the web service
+	 * @param args not used
+	 */
+	public static void main(String[] args) {		
+		port(8088);
+		
+		//Simple route so you can check things are working...
+		//Accessible via http://localhost:8088/test in your browser
+		get("/test", new Route() {
+			@Override
+			public Object handle(Request request, Response response) throws Exception {
+				try (DB db = new DB()) {
+					return "Number of Entries: " + db.getNumberOfEntries();
+				}
+			}			
+		});
+		
+		System.out.println("Server up! Don't forget to kill the program when done!");
+	}
+
+}
