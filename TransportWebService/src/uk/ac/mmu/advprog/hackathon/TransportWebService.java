ad5c57fef8904ce833efc7e18be09040459d27ec
Ben Walker <63571454+walkerrobertben@users.noreply.github.com>
2023-01-09 15:31:33 +0000

    Added code overview to main class

--- TransportWebService/src/uk/ac/mmu/advprog/hackathon/TransportWebService.java
+++ TransportWebService/src/uk/ac/mmu/advprog/hackathon/TransportWebService.java
@@ -3,7 +3,28 @@ package uk.ac.mmu.advprog.hackathon;
 import spark.Spark;
 
 /*
- * Main class 
+ * Main class
+ * 
+ * Code overview:
+ * 
+ * Main class (this one) starts spark server and binds routes
+ * 
+ * Route controllers are in the uk.ac.mmu.advprog.hackathon.routes package
+ * Each route is handled in an individual file
+ * 
+ * Route handlers:
+ *  - validate parameters (validation checks in the Validation.java file)
+ *  - ask the DB.java class for the data
+ *  - return
+ *  
+ *  DB.java handles connecting, reading, and closing, the sqlite connection
+ *  For routes that return stops (/stops and /nearest) the db rows are parsed into a NaptanStops
+ *  
+ *  NaptanStops object contains an arraylist of NaptanStop objects
+ *  NaptanStop objects represent a single stop
+ *  
+ *  NaptanStops object provides asJson() and asXML() methods used by routes to build strings.
+ * 
  */
 public class TransportWebService {
 	
